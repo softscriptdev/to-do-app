e4c98e75f514cee9f4ee1f5af6d1107afa3ea40b
Anna Holzer <r.holzer089@gmail.com>
2025-05-30 12:11:24 +0000

    save to dos to localStorage

--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -3,12 +3,17 @@ import CreateToDo from "./Header";
 import ShowToDo from "./ShowToDo";
 
 export default function ToDoApp() {
-  const [gettodos, addtodos] = useState([]);
+  /* NOTE set state for to dos | get to dos from local storage */
+  const [gettodos, addtodos] = useState(
+    () => JSON.parse(localStorage.getItem("todos")) || []
+  );
 
+  /* NOTE save to dos to local storage */
   useEffect(() => {
-    console.log(gettodos);
+    localStorage.setItem("todos", JSON.stringify(gettodos));
   }, [gettodos]);
 
+  /* NOTTE add to do to state */
   const addtodo = (todo) => {
     /* NOTE to do: [note, done] */
     addtodos((prev) => [...prev, [todo, false]]);
