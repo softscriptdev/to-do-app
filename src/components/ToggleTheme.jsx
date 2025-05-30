bb182caff7df3e4b249c3bbc4a697170ab536e82
Anna Holzer <r.holzer089@gmail.com>
2025-05-30 11:15:27 +0000

    add comments to ToggleTheme

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -5,17 +5,20 @@ import { useEffect, useState } from "react";
 import style from "../style/ToggleTheme.module.scss";
 
 export default function ToggleTheme() {
+  /* NOTE set body to theme */
   const setbodytheme = (theme) => {
     document.body.classList?.add(theme == "light" ? "light" : "dark");
     document.body.classList?.remove(theme == "light" ? "dark" : "light");
   };
 
+  /* NOTE get theme from local storage and set state */
   const [gettheme, settheme] = useState(() => {
     let theme = localStorage.getItem("ToDoApp-Theme") || "dark";
     setbodytheme(theme);
     return theme;
   });
 
+  /* NOTE get theme from local storage */
   useEffect(() => {
     localStorage.setItem("ToDoApp-Theme", gettheme);
     setbodytheme(gettheme);
@@ -26,10 +29,10 @@ export default function ToggleTheme() {
       className={style.togglestyle}
       size="xl"
       icon={gettheme === "dark" ? faSun : faMoon}
+      color={gettheme === "dark" ? "#e0e7e9" : "#3f4c4d"}
       onClick={() =>
         settheme((prevtheme) => (prevtheme === "dark" ? "light" : "dark"))
       }
-      color={gettheme === "dark" ? "#e0e7e9" : "#3f4c4d"}
     />
   );
 }
