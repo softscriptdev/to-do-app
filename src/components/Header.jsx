4ebdb366d4d017b133a55a890cbf60cbfe81ce6b
Anna Holzer <r.holzer089@gmail.com>
2025-05-30 11:28:20 +0000

    functionality to create a new to do

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import style from "../style/CreateToDo.module.scss";
 import { faPlus, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
 
-export default function CreateToDo() {
+export default function CreateToDo({ addtodo }) {
+  /* NOTE state to show or hide create todo */
   const [create, changecreate] = useState(false);
+  /* NOTE current To Do in the input to create a new todo */
   const [newtodo, changetodo] = useState(null);
 
-  useEffect(() => {
-    console.log(newtodo);
-  }, [newtodo]);
-
-  
+  /* NOTE check current to do and add it to the todo list */
+  const checknewtodo = () => {
+    let todo = newtodo?.trim();
+    if (todo) {
+      addtodo(todo);
+      changetodo(null);
+      changecreate(false);
+    }
+  };
 
   return (
     <>
+      {/* NOTE header with btn to create a new todo */}
       <header className={style.header}>
         <h1>My Tasks</h1>
         <FontAwesomeIcon
@@ -25,6 +32,8 @@ export default function CreateToDo() {
           color="#d85757"
         />
       </header>
+
+      {/* NOTE create a new todo */}
       {create && (
         <div className={style.creattodo}>
           <input
@@ -32,7 +41,12 @@ export default function CreateToDo() {
             onChange={(e) => changetodo(e.target.value.trim())}
             placeholder="My new task ..."
           />
-          <FontAwesomeIcon icon={faSquarePlus} size="xl" color="#d85757" />
+          <FontAwesomeIcon
+            icon={faSquarePlus}
+            size="xl"
+            color="#d85757"
+            onClick={checknewtodo}
+          />
         </div>
       )}
     </>
--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CreateToDo from "./Header";
 
 export default function ToDoApp() {
   const [gettodos, addtodos] = useState([]);
 
+  useEffect(() => {
+    console.log(gettodos);
+  }, [gettodos]);
+
+  const addtodo = (todo) => {
+    /* NOTE to do: [note, done] */
+    addtodos((prev) => [...prev, [todo, false]]);
+  };
+
   return (
     <article>
-      <CreateToDo />
+      <CreateToDo addtodo={addtodo} />
     </article>
   );
 }
