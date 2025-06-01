2c1f81169a0ce48d2cb9ce881d9cf362e4a6efb9
Anna Holzer <r.holzer089@gmail.com>
2025-06-01 08:50:33 +0000

    add fonts to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import ToggleTheme from "./components/ToggleTheme";
 import ToDoApp from "./components/ToDoApp";
+import "./style/main.scss";
+import "./style/fonts.scss";
 
 function App() {
   return (
