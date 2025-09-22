5d221e35d8b87876edb9cf091d5a441cc4a687f8
Anna Holzer <r.holzer089@gmail.com>
2025-09-22 13:04:19 +0200

    Theme handler for the theme

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -1,26 +1,12 @@
 import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { ThemeContext } from "../hooks/ThemeHandler";
+
 
 export default function ToggleTheme() {
-  /* NOTE set body to theme */
-  const setbodytheme = (theme) => {
-    document.body.classList?.add(theme == "light" ? "light" : "dark");
-    document.body.classList?.remove(theme == "light" ? "dark" : "light");
-  };
-
-  /* NOTE get theme from local storage and set state */
-  const [gettheme, settheme] = useState(() => {
-    let theme = localStorage.getItem("ToDoApp-Theme") || "dark";
-    setbodytheme(theme);
-    return theme;
-  });
-
-  /* NOTE get theme from local storage */
-  useEffect(() => {
-    localStorage.setItem("ToDoApp-Theme", gettheme);
-    setbodytheme(gettheme);
-  }, [gettheme]);
+  /* NOTE get theme and set theme from context */
+  const { gettheme, settheme } = useContext(ThemeContext);
 
   return (
     <div>
